59e1d4bed20a2c418ebd15c8fe9d4c39896aeffc
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-05-08 22:45:01 +0300

    Updating obstacle indicator modulate

--- actors/obstacle/obstacle.gd
+++ actors/obstacle/obstacle.gd
@@ -14,17 +14,51 @@ extends Node3D
 	get:
 		return indicator_value
 	set(value):
-		indicator.position.y = (indicator_max_height - indicator_min_height) * indicator_value + indicator_min_height
+		# Adjusting indicator position
+		indicator.position.y = (INDICATOR_MAX - INDICATOR_MIN) * indicator_value + INDICATOR_MIN
+		
+		# Updating indicator color
+		match get_indicator_position():
+			IndicatorPosition.Default:
+				indicator.modulate = Color.WHITE
+			IndicatorPosition.Fail:
+				indicator.modulate = Color.RED
+			IndicatorPosition.Perfect:
+				indicator.modulate = Color.BLUE
+			IndicatorPosition.TooHigh:
+				indicator.modulate = Color.SLATE_GRAY
+		
 		indicator_value = value
-
-@export_category("Indicator settings")
-@export var indicator_max_height: float = 2.5
-@export var indicator_min_height: float = 0.2
 
 @export_category("Node references")
 @export var indicator: Sprite3D
 @export var poles: Array[Pole]
 
+enum IndicatorPosition
+{
+	Default,
+	Fail,
+	Perfect,
+	TooHigh
+}
+
+const INDICATOR_OFFSET: float = 0.35
+const INDICATOR_MAX: float = 3
+const INDICATOR_MIN: float = 0.2
+const PERFECT_RANGE: float = 0.3
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+
+func get_indicator_position() -> IndicatorPosition:
+	if indicator.position.y < poles[0].position.y + .001:
+		return IndicatorPosition.Default
+	elif indicator.position.y < poles[height-1].position.y + INDICATOR_OFFSET:
+		return IndicatorPosition.Fail
+	elif indicator.position.y < poles[height-1].position.y + PERFECT_RANGE + INDICATOR_OFFSET:
+		return IndicatorPosition.Perfect
+	else:
+		return IndicatorPosition.TooHigh
