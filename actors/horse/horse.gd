c2dd15e5de443b33c58e532152f63b32bddb0242
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-06-01 15:56:32 +0300

    Implemented loading camera and particle settings to horse.gd
    - Fixed bug with clamping that camera would get "stuck" when continuously moving it over border

--- actors/horse/horse.gd
+++ actors/horse/horse.gd
@@ -7,6 +7,22 @@ class_name Horse extends CharacterBody3D
 @onready var obstacle_crash_area: Area3D = $ObstacleCrashArea
 @onready var perfect_particles: CPUParticles3D = $PerfectParticles
 
+enum Gaits
+{
+	Backward,
+	Stop,
+	Walk,
+	Trot,
+	Canter,
+	Gallop
+}
+
+const CAMERA_CLAMP_DEGREES_X := 45
+const TURNING_SPEED := 3
+const JUMP_CHARGE_SPEED := 5
+const JUMP_VELOCITY := 2
+const JUMP_HEIGHT_OFFSET := 0.4
+
 var is_jumping: bool:
 	get:
 		return jump_curve.point_count != 0
@@ -23,38 +39,57 @@ var current_obstacle: Obstacle = null
 
 var timer: float = 0
 var jump_charge: float = 0
+var jump_landing_pos: Vector3 = Vector3.ZERO
 
-const TURNING_SPEED := 3
-const CAMERA_SPEED := 0.8
-const JUMP_CHARGE_SPEED := 5
-const JUMP_VELOCITY := 2
-const JUMP_HEIGHT_OFFSET := 0.4
+var camera_speed_x: float = 0.8
+var camera_speed_y: float = 0.8
 
-enum Gaits
-{
-	Backward,
-	Stop,
-	Walk,
-	Trot,
-	Canter,
-	Gallop
-}
+var camera_invert_x: bool = false
+var camera_invert_y: bool = false
 
 var camera_rotation: Vector2 = Vector2.ZERO
-var jump_landing_pos: Vector3 = Vector3.ZERO
+
+var can_emit_perfect_particles: bool = true
 
 
 func _enter_tree() -> void:
 	EventSystem.OBS_jumping_area_entered.connect(on_jumping_area_entered)
 	EventSystem.OBS_jumping_area_exited.connect(on_jumping_area_exited)
 	EventSystem.OBS_perfect_points_earned.connect(on_perfect_points_earned)
+	
+	# Connecting signals for camera setting updates
+	EventSystem.SET_set_camera_speed_x.connect(func(new_speed: float): camera_speed_x = new_speed)
+	EventSystem.SET_set_camera_speed_y.connect(func(new_speed: float): camera_speed_y = new_speed)
+	EventSystem.SET_toggle_camera_invert_x.connect(func(invert: bool): camera_invert_x = invert)
+	EventSystem.SET_toggle_camera_invert_y.connect(func(invert: bool): camera_invert_y = invert)
+	
+	# Connecting signal for perfect particle setting updates
+	EventSystem.SET_toggle_perfect_jump_particles.connect(func(toggle: bool): can_emit_perfect_particles = toggle)
 
 
 func _ready() -> void:
+	# Signal to open hud
 	EventSystem.UI_open_menu.emit(UiReference.Keys.PlayerHud)
 	
-	# Emitting the particles so that they don't cause lag afterwards
-	perfect_particles.emitting = true
+	# Getting camera settings from SettingsManager
+	if SettingsManager.Settings.has(SettingsManager.CAMERA_SPEED_X_KEY):
+		camera_speed_x = SettingsManager.Settings.get(SettingsManager.CAMERA_SPEED_X_KEY)
+	
+	if SettingsManager.Settings.has(SettingsManager.CAMERA_SPEED_Y_KEY):
+		camera_speed_y = SettingsManager.Settings.get(SettingsManager.CAMERA_SPEED_Y_KEY)
+	
+	if SettingsManager.Settings.has(SettingsManager.CAMERA_INVERT_X_KEY):
+		camera_invert_x = SettingsManager.Settings.get(SettingsManager.CAMERA_INVERT_X_KEY)
+	
+	if SettingsManager.Settings.has(SettingsManager.CAMERA_INVERT_Y_KEY):
+		camera_invert_y = SettingsManager.Settings.get(SettingsManager.CAMERA_INVERT_Y_KEY)
+	
+	# Getting perfect particle setting from SettingsManager
+	if SettingsManager.Settings.has(SettingsManager.PERFECT_JUMP_PARTICLES_KEY):
+		can_emit_perfect_particles = SettingsManager.Settings.get(SettingsManager.PERFECT_JUMP_PARTICLES_KEY)
+	
+	# Emitting the particles so that they don't cause lag on the first jump
+	perfect_particles.emitting = can_emit_perfect_particles
 
 
 func set_starting_pos(pos_global: Vector3, rotation_global: Vector3) -> void:
@@ -75,21 +110,36 @@ func set_starting_pos(pos_global: Vector3, rotation_global: Vector3) -> void:
 
 
 func _input(event: InputEvent) -> void:
+	# Pressing escape opens pause menu
 	if event.is_action_pressed("escape"):
 		EventSystem.UI_open_menu.emit(UiReference.Keys.PauseMenu)
-		
-	if event is InputEventMouseMotion:
-		camera_rotation.x -= event.relative.y * CAMERA_SPEED
-		camera_rotation.y -= event.relative.x * CAMERA_SPEED
 	
+	if event is InputEventMouseMotion:
+		# Calculating how much camera should move and checking if should move inverted
+		if camera_invert_x:
+			camera_rotation.x += event.relative.y * camera_speed_x
+		else:
+			camera_rotation.x -= event.relative.y * camera_speed_x
+		
+		if camera_invert_y:
+			camera_rotation.y += event.relative.x * camera_speed_y
+		else:
+			camera_rotation.y -= event.relative.x * camera_speed_y
+		
+		# Clamping camera rotation
+		camera_rotation.x = clamp(camera_rotation.x, -CAMERA_CLAMP_DEGREES_X, CAMERA_CLAMP_DEGREES_X)
+	
+	# Increasing gait if pressing forward
 	if event.is_action_pressed("forward") && can_move:
 		current_gait = clamp(current_gait + 1, Gaits.Backward, Gaits.Gallop)
 		adjust_speed()
 	
+	# Decreasing gait if pressing backward
 	if event.is_action_pressed("backward") && can_move:
 		current_gait = clamp(current_gait - 1, Gaits.Backward, Gaits.Gallop)
 		adjust_speed()
 	
+	# If mouse is not captured when screen is clicked capture it
 	if event is InputEventMouseButton and Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -126,7 +176,7 @@ func on_jumping_area_exited():
 
 
 func on_perfect_points_earned():
-	perfect_particles.emitting = true
+	perfect_particles.emitting = can_emit_perfect_particles
 
 
 func calculate_jump_curve():
@@ -208,7 +258,6 @@ func _physics_process(delta: float) -> void:
 	
 	# Rotate and move camera
 	spring_arm.rotation_degrees.x = camera_rotation.x
-	spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
 	pivot.rotation_degrees.y = camera_rotation.y
 	pivot.global_position.x = global_position.x
 	pivot.global_position.z = global_position.z
