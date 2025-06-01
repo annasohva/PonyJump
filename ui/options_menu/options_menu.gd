0a49ea1442950be3c71ec2ce28cea1704df651e2
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-06-01 10:53:00 +0300

    Created the UI for options menu, implemented switching tabs in script

--- ui/options_menu/options_menu.gd
+++ ui/options_menu/options_menu.gd
@@ -1,5 +1,24 @@
 extends Control
 
 
+@onready var gameplay_options: Control = $BackgroundPanel/GameplayOptions
+@onready var audio_options: Control = $BackgroundPanel/AudioOptions
+@onready var graphics_options: Control = $BackgroundPanel/GraphicsOptions
+
+
+enum TabKeys
+{
+	Gameplay,
+	Audio,
+	Graphics
+}
+
+
 func _on_close_button_pressed() -> void:
 	queue_free()
+
+
+func _on_tab_bar_tab_changed(tab: int) -> void:
+	gameplay_options.visible = tab == TabKeys.Gameplay
+	audio_options.visible = tab == TabKeys.Audio
+	graphics_options.visible = tab == TabKeys.Graphics
