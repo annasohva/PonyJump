ad6b3c611fe734ca7d91b508696056d0aab6def9
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-06-01 16:27:23 +0300

    Removed unused methods from settings manager

--- game/managers/settings_manager.gd
+++ game/managers/settings_manager.gd
@@ -69,19 +69,6 @@ func _enter_tree() -> void:
 func _ready():
 	var settings_dict = Settings
 	
-	# Loading the saved Audio settings
-	if settings_dict.has(GENERAL_VOLUME_KEY):
-		set_general_volume(settings_dict.get(GENERAL_VOLUME_KEY))
-	
-	if settings_dict.has(MUSIC_VOLUME_KEY):
-		set_music_volume(settings_dict.get(MUSIC_VOLUME_KEY))
-	
-	if settings_dict.has(SFX_VOLUME_KEY):
-		set_sfx_volume(settings_dict.get(SFX_VOLUME_KEY))
-	
-	if settings_dict.has(AMBIENCE_VOLUME_KEY):
-		set_ambience_volume(settings_dict.get(AMBIENCE_VOLUME_KEY))
-	
 	# Loading the saved Graphics settings which need to be applied from this script
 	if settings_dict.has(GAME_WINDOW_KEY) and !OS.has_feature("web"):
 		set_game_window(settings_dict.get(GAME_WINDOW_KEY))
@@ -112,87 +99,42 @@ func save_settings() -> void:
 	settings_file.close()
 
 
-# Handle methods save the setting and call the setting method
+# Handle methods save the setting and call the implementation method if there is one in this script
+
 func handle_set_camera_speed_x(new_speed: float) -> void:
 	Settings[CAMERA_SPEED_X_KEY] = new_speed
-	set_camera_speed_x(new_speed)
-
-
-# Setting methods implement the setting's functionality
-func set_camera_speed_x(new_speed: float) -> void:
-	pass
 
 
 func handle_set_camera_speed_y(new_speed: float) -> void:
 	Settings[CAMERA_SPEED_Y_KEY] = new_speed
-	set_camera_speed_y(new_speed)
-
-
-func set_camera_speed_y(new_speed: float) -> void:
-	pass
 
 
 func handle_set_camera_invert_x(invert: bool) -> void:
 	Settings[CAMERA_INVERT_X_KEY] = invert
-	set_camera_invert_x(invert)
-
-
-func set_camera_invert_x(invert: bool) -> void:
-	pass
 
 
 func handle_set_camera_invert_y(invert: bool) -> void:
 	Settings[CAMERA_INVERT_Y_KEY] = invert
-	set_camera_invert_y(invert)
-
-
-func set_camera_invert_y(invert: bool) -> void:
-	pass
 
 
 func handle_toggle_perfect_jump_particles(show: bool) -> void:
 	Settings[PERFECT_JUMP_PARTICLES_KEY] = show
-	toggle_perfect_jump_particles(show)
-
-
-func toggle_perfect_jump_particles(show: bool) -> void:
-	pass
 
 
 func handle_set_general_volume(new_volume: float) -> void:
 	Settings[GENERAL_VOLUME_KEY] = new_volume
-	set_general_volume(new_volume)
-
-
-func set_general_volume(new_volume: float) -> void:
-	pass
 
 
 func handle_set_music_volume(new_volume: float) -> void:
 	Settings[MUSIC_VOLUME_KEY] = new_volume
-	set_music_volume(new_volume)
-
-
-func set_music_volume(new_volume: float) -> void:
-	pass
 
 
 func handle_set_sfx_volume(new_volume: float) -> void:
 	Settings[SFX_VOLUME_KEY] = new_volume
-	set_sfx_volume(new_volume)
-
-
-func set_sfx_volume(new_volume: float) -> void:
-	pass
 
 
 func handle_set_ambience_volume(new_volume: float) -> void:
 	Settings[AMBIENCE_VOLUME_KEY] = new_volume
-	set_ambience_volume(new_volume)
-
-
-func set_ambience_volume(new_volume: float) -> void:
-	pass
 
 
 func handle_set_game_window(new_setting: OptionsMenu.GameWindowOptions) -> void:
@@ -200,17 +142,45 @@ func handle_set_game_window(new_setting: OptionsMenu.GameWindowOptions) -> void:
 	set_game_window(new_setting)
 
 
+func handle_set_fps(new_setting: OptionsMenu.FpsOptions) -> void:
+	Settings[FPS_KEY] = new_setting
+	set_fps(new_setting)
+
+
+func handle_toggle_vsync(toggle: bool) -> void:
+	Settings[VSYNC_KEY] = toggle
+	toggle_vsync(toggle)
+
+
+func handle_set_anti_aliasing(new_setting: OptionsMenu.AntiAliasingOptions) -> void:
+	Settings[ANTI_ALIASING_KEY] = new_setting
+	set_anti_aliasing(new_setting)
+
+
+func handle_toggle_shadows(show: bool) -> void:
+	Settings[SHADOWS_KEY] = show
+
+
+func handle_toggle_trees(show: bool) -> void:
+	Settings[TREES_KEY] = show
+
+
+func handle_toggle_foliage(show: bool) -> void:
+	Settings[FOLIAGE_KEY] = show
+
+
+func handle_toggle_nature_props(show: bool) -> void:
+	Settings[NATURE_PROPS_KEY] = show
+
+
+# Implementation methods
+
 func set_game_window(new_setting: OptionsMenu.GameWindowOptions) -> void:
 	match new_setting:
 		OptionsMenu.GameWindowOptions.Fullscreen:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		OptionsMenu.GameWindowOptions.Windowed:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
-
-
-func handle_set_fps(new_setting: OptionsMenu.FpsOptions) -> void:
-	Settings[FPS_KEY] = new_setting
-	set_fps(new_setting)
 
 
 func set_fps(new_setting: OptionsMenu.FpsOptions) -> void:
@@ -227,21 +197,11 @@ func set_fps(new_setting: OptionsMenu.FpsOptions) -> void:
 			Engine.max_fps = 144
 
 
-func handle_toggle_vsync(toggle: bool) -> void:
-	Settings[VSYNC_KEY] = toggle
-	toggle_vsync(toggle)
-
-
 func toggle_vsync(toggle: bool) -> void:
 	if toggle:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
 	else:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-
-
-func handle_set_anti_aliasing(new_setting: OptionsMenu.AntiAliasingOptions) -> void:
-	Settings[ANTI_ALIASING_KEY] = new_setting
-	set_anti_aliasing(new_setting)
 
 
 func set_anti_aliasing(new_setting: OptionsMenu.AntiAliasingOptions) -> void:
@@ -256,39 +216,3 @@ func set_anti_aliasing(new_setting: OptionsMenu.AntiAliasingOptions) -> void:
 			viewport_msaa = Viewport.MSAA_8X
 	
 	get_viewport().msaa_3d = viewport_msaa
-
-
-func handle_toggle_shadows(show: bool) -> void:
-	Settings[SHADOWS_KEY] = show
-	toggle_shadows(show)
-
-
-func toggle_shadows(show: bool) -> void:
-	pass
-
-
-func handle_toggle_trees(show: bool) -> void:
-	Settings[TREES_KEY] = show
-	toggle_trees(show)
-
-
-func toggle_trees(show: bool) -> void:
-	pass
-
-
-func handle_toggle_foliage(show: bool) -> void:
-	Settings[FOLIAGE_KEY] = show
-	toggle_foliage(show)
-
-
-func toggle_foliage(show: bool) -> void:
-	pass
-
-
-func handle_toggle_nature_props(show: bool) -> void:
-	Settings[NATURE_PROPS_KEY] = show
-	toggle_nature_props(show)
-
-
-func toggle_nature_props(show: bool) -> void:
-	pass
